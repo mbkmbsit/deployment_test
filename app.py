41b31fe673b167f4e2f857185595499acd028d97
mbk <mbkmbsit@gmail.com>
2021-02-16 22:08:28 +0530

    procfile, pipfile added, app updated for list envs

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
+import os
+
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -8,4 +10,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return jsonify(app=f'{app.__dict__}', bilal='testing')
+    return jsonify(app=f'{app.__dict__}', bilal='testing', envs=dict(os.environ))
